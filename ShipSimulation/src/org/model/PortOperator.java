de8b3e038565b4e396f5a8b6517b3f528d29f1cf
Shinnosuke Wanaka <wanaka@is.k.u-tokyo.ac.jp>
2017-04-25 20:33:56 -0400

    develop class about cash flow
    Signed-off-by: Shinnosuke Wanaka <wanaka@is.k.u-tokyo.ac.jp>

--- ShipSimulation/src/org/model/PortOperator.java
+++ ShipSimulation/src/org/model/PortOperator.java
@@ -1,5 +1,39 @@
 package org.model;
 
 public abstract class PortOperator {
+	//Configuration
+	private String name;
+	private double fixedCost;
 
+	//Status
+	private double cashFlow;
+	
+	//Function
+	public void addCashFlow(double add){
+		this.cashFlow += add;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getFixedCost() {
+		return fixedCost;
+	}
+
+	public void setFixedCost(double fixedCost) {
+		this.fixedCost = fixedCost;
+	}
+
+	public double getCashFlow() {
+		return cashFlow;
+	}
+
+	public void setCashFlow(double cashFlow) {
+		this.cashFlow = cashFlow;
+	}
 }
--- ShipSimulation/src/org/model/ShipOperator.java
+++ ShipSimulation/src/org/model/ShipOperator.java
@@ -1,5 +1,50 @@
 package org.model;
 
 public abstract class ShipOperator {
+	//Configuration
+	private String name;
+	private Fleet fleet;
+	private double fixedCost;
+
+	//Status
+	private double cashFlow;
+	
+	//Function
+	public void addCashFlow(double add){
+		this.cashFlow += add;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Fleet getFleet() {
+		return fleet;
+	}
+
+	public void setFleet(Fleet fleet) {
+		this.fleet = fleet;
+	}
+
+	public double getFixedCost() {
+		return fixedCost;
+	}
+
+	public void setFixedCost(double fixedCost) {
+		this.fixedCost = fixedCost;
+	}
+
+	public double getCashFlow() {
+		return cashFlow;
+	}
+
+	public void setCashFlow(double cashFlow) {
+		this.cashFlow = cashFlow;
+	}
+
 
 }
